ce1f5fc474019a2f7b05147f322853b1ce3643a0
j-winston <kenjameswinston@gmail.com>
2023-01-27 07:24:55 -0500

    the i-forgot commit

--- nvim/.config/nvim/lua/lsp_config.lua
+++ nvim/.config/nvim/lua/lsp_config.lua
@@ -35,3 +35,29 @@ local servers = { 'pyright', 'emmet_ls', 'tsserver', 'cssls' }
             on_attach = on_attach
         }
     end
+
+--emmet_ls 
+
+local lspconfig = require('lspconfig')
+local configs = require('lspconfig/configs')
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+lspconfig.emmet_ls.setup({
+    -- on_attach = on_attach,
+    capabilities = capabilities,
+    filetypes = { 'html', 'typescriptreact', 'javascriptreact', 'css', 'sass', 'scss', 'less' },
+    init_options = {
+      html = {
+        options = {
+          -- For possible options, see: https://github.com/emmetio/emmet/blob/master/src/config.ts#L79-L267
+          ["bem.enabled"] = true,
+          ["output.inlineBreak"] = 1,
+          ["inlineElements"] = {'a', 'code'},
+          ["output.formatLeafNode"] = true,
+
+        },
+      },
+    }
+})
+-----End emmet_ls
