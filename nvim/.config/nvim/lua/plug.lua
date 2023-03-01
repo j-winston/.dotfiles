2944f6bf9293545634f25f2cdeb28590e79c9d55
j-winston <kenjameswinston@gmail.com>
2023-03-01 06:48:09 -0500

    add nvim surround plugin to plug.lua

--- nvim/.config/nvim/lua/plug.lua
+++ nvim/.config/nvim/lua/plug.lua
@@ -63,6 +63,18 @@ return require('packer').startup(function(use)
     use { 'onsails/lspkind.nvim'}
     use { 'tpope/vim-fugitive' }
 
+    --[[Surround]]
+
+    use({
+    "kylechui/nvim-surround",
+    tag = "*", -- Use for stability; omit to use `main` branch for the latest features
+    config = function()
+        require("nvim-surround").setup({
+            -- Configuration here, or leave empty to use defaults
+        })
+    end
+
+})
       if packer_bootstrap then
         require('packer').sync()
       end
