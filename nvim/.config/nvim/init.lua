9dcd621f941357536439b015b21d2eb95263304e
j-winston <kenjameswinston@gmail.com>
2023-07-03 07:47:56 -0400

    update

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,6 +1,9 @@
 --[[ init.lua ]]
 
 
+-- Disable netrw 
+vim.g.loaded_netrw = 1 
+vim.g.loaded_netrwPlugin = 1 
 -- LEADER
 -- These keybindings need to be defined before the first / 
 -- is called; otherwise, it will default to "\"
